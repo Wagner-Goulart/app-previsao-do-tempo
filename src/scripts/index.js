28e35d1d2b6bb805c73616df95ee71b1f98f6bdb
https://github.com/Wagner-Goulart <helga.goulart@jrcomercio.net>
2023-05-02 22:18:13 -0300

    Adiconada Mapa e validação de pesquisa quando vazio

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,37 +1,43 @@
 const key = '4cb5a31adc4fff6205dbb26aa82448a3';
 const cityBtn = document.querySelector('.city-btn')
+var map
 
-cityBtn.addEventListener('click', async ()=> {
+cityBtn.addEventListener('click', async () => {
     const inputCity = document.querySelector('.input-city').value
     const weatherResults = document.querySelector('.weather-results')
+
+    if(!inputCity) {
+        alert('Informe uma cidade')
+    }
 
     const city = await getWeather(inputCity)
     const cityName = (city.name)
     const cityCountry = (city.sys.country)
     const cityLat = (city.coord.lat)
     const cityLon = (city.coord.lon)
-    const Temperature = (city.main.temp)
-    const minTemp = (city.main.temp_min)
-    const FeelsLike = (city.main.feels_like)
+    const Temperature = Math.floor((city.main.temp))
+    const minTemp = Math.floor((city.main.temp_min))
+    const FeelsLike = Math.floor((city.main.feels_like))
     const icon = (city.weather[0].icon)
-    
+   
+
     weatherResults.innerHTML = `<div class = 'weather-info'>
-                                <h2 class = "city-tittle">${cityName}, 
-                                    <span class = "city-country">${cityCountry}
-                                    </span>
-                                </h2>
-                                <small class = "lat-long">${cityLat} ${cityLon}</small>
-                                <p class = "temperature">
-                                    <img src = "https://openweathermap.org/img/wn/${icon}@2x.png">${Temperature} ºC 
-                                    <small class = "min-temp">Min:${minTemp} ºC</small>
-                                </p>
-                                <p class = "feels-like">Sensação térmica: ${FeelsLike} ºC</p>
+                                    <h2 class = "city-tittle">${cityName}, 
+                                        <span class = "city-country">${cityCountry}
+                                        </span>
+                                    </h2>
+                                    <small class = "lat-long">${cityLat} ${cityLon}</small>
+                                    <p class = "temperature">
+                                        <img src = "https://openweathermap.org/img/wn/${icon}@2x.png">${Temperature} ºC 
+                                        <small class = "min-temp">Min:${minTemp} ºC</small>
+                                    </p>
+                                    <p class = "feels-like">Sensação térmica: ${FeelsLike} ºC</p>
                                 </div>
                                 `
 })
 
-async function getCity(cityName){
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},&limit=${1}&appid=${key}`)
+async function getCity(cityName) {
+    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName},&limit=${1}&appid=4cb5a31adc4fff6205dbb26aa82448a3`)
 
     return await response.json()
 
@@ -39,10 +45,35 @@ async function getCity(cityName){
 
 async function getWeather(cityName) {
     const latLong = await getCity(cityName)
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latLong[0].lat}&lon=${latLong[0].lon}&units=metric&appid=${key}`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latLong[0].lat}&lon=${latLong[0].lon}&units=metric&appid=4cb5a31adc4fff6205dbb26aa82448a3`)
+
+    renderMap(cityName)
 
     return await response.json()
 }
+
+async function renderMap(cityName) {
+    const latLong = await getCity(cityName)
+
+    if (map === undefined) {
+        map = L.map('map').setView([latLong[0].lat,latLong[0].lon], 13);
+    } else {
+        map.remove()
+        map = L.map('map').setView([latLong[0].lat,latLong[0].lon], 13);
+    }
+   
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([latLong[0].lat,latLong[0].lon]).addTo(map)
+        .openPopup();
+
+}
+
+
+
+
 
 
 
